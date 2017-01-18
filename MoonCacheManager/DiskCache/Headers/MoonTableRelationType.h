accb20f0f1cc1071d53970d7f17b2ca9005777cc
1396547287 <1396547287@qq.com>
2017-01-18 16:17:02 +0800

    add table relation type enum

---
+++ MoonCacheManager/DiskCache/Headers/MoonTableRelationType.h
@@ -0,0 +1,19 @@
+//
+//  MoonTableRelationType.h
+//  MoonCacheManagerDemo
+//
+//  Created by jianchengpan on 2017/1/18.
+//  Copyright © 2017年 jianchengpan. All rights reserved.
+//
+
+#ifndef MoonTableRelationType_h
+#define MoonTableRelationType_h
+
+typedef NS_ENUM(NSUInteger, MoonTableRelationType) {
+    MoonTableRelationTypeUnknown    = 0,
+    MoonTableRelationTypeHasOne     = 1,
+    MoonTableRelationTypeHasMany    = 2,
+    MoonTableRelationTypeBelongsTo  = 3,
+};
+
+#endif /* MoonTableRelationType_h */
