9fd31a89d3ea8c14385f28dd6f598a5ce7ccb16f
1396547287 <1396547287@qq.com>
2017-01-18 12:03:40 +0800

    create project

---
+++ MoonCacheManagerDemo/MoonCacheManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoonCacheManagerDemo
+//
+//  Created by jianchengpan on 2017/1/18.
+//  Copyright © 2017年 jianchengpan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
