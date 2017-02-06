e281be2fd0322a4267d6c032f3c07d85de39af33
1396547287 <1396547287@qq.com>
2017-02-06 12:00:36 +0800

    add sqlMaker protocol

---
+++ MoonCacheManager/DiskCache/Protocols/MoonSqlMakerProtocol.h
@@ -0,0 +1,13 @@
+//
+//  MoonSqlMakerProtocol.h
+//  MoonCacheManagerDemo
+//
+//  Created by jianchengpan on 2017/2/6.
+//  Copyright © 2017年 jianchengpan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MoonSqlMakerProtocol <NSObject>
+
+@end
